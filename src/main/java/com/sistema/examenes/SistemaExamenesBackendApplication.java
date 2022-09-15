a1a593c4208d18c83b95eb17abecf386dac34a84
baruch hdz <baruchhdz@baruchs-iMac-93.home>
2022-09-15 01:39:22 -0400

    password encriptada y limpiar y crear nuevamente base de datos

--- src/main/java/com/sistema/examenes/SistemaExamenesBackendApplication.java
+++ src/main/java/com/sistema/examenes/SistemaExamenesBackendApplication.java
@@ -1,16 +1,17 @@
 package com.sistema.examenes;
 
-//import java.util.HashSet;
-//import java.util.Set;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-//import com.sistema.examenes.entidades.Rol;
-//import com.sistema.examenes.entidades.Usuario;
-//import com.sistema.examenes.entidades.UsuarioRol;
+import com.sistema.examenes.entidades.Usuario;
+import com.sistema.examenes.entidades.Rol;
+import com.sistema.examenes.entidades.UsuarioRol;
 import com.sistema.examenes.servicios.UsuarioService;
 
 @SpringBootApplication
@@ -18,6 +19,9 @@ public class SistemaExamenesBackendApplication implements CommandLineRunner{
 	
 	@Autowired
 	private UsuarioService usuarioService;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaExamenesBackendApplication.class, args);
@@ -26,29 +30,28 @@ public class SistemaExamenesBackendApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-//	Usuario usuario = new Usuario();	
-//	
-//	usuario.setNombre("arturo");
-//	usuario.setApellidos("hdez");
-//	usuario.setUsername("arturo");
-//	usuario.setPassword("arturo");
-//	usuario.setEmail("arturo@gmail.com");
-//	usuario.setTelefono("1234567890");
-//	usuario.setPerfil("foto.png");
-//	
-//	Rol rol = new Rol();
-//	rol.setRolId(1L);
-//	rol.setNombre("ADMIN");
-//	
-//	Set<UsuarioRol> usuarioRoles = new HashSet<>();
-//	UsuarioRol usuarioRol = new UsuarioRol();
-//	usuarioRol.setRol(rol);
-//	usuarioRol.setUsuario(usuario);
-//	usuarioRoles.add(usuarioRol);
-//	
-//	Usuario usuarioGuardado = usuarioService.guardarUsuario(usuario, usuarioRoles);
-//	System.out.println(usuarioGuardado.getUsername());
+	Usuario usuario = new Usuario();	
 	
+	usuario.setNombre("arturo");
+	usuario.setApellidos("hdez");
+	usuario.setUsername("arturo");
+	usuario.setPassword(bCryptPasswordEncoder.encode("arturo"));
+	usuario.setEmail("arturo@gmail.com");
+	usuario.setTelefono("1234567890");
+	usuario.setPerfil("foto.png");
+	
+	Rol rol = new Rol();
+	rol.setRolId(1L);
+	rol.setNombre("ADMIN");
+	
+	Set<UsuarioRol> usuarioRoles = new HashSet<>();
+	UsuarioRol usuarioRol = new UsuarioRol();
+	usuarioRol.setRol(rol);
+	usuarioRol.setUsuario(usuario);
+	usuarioRoles.add(usuarioRol);
+	
+	Usuario usuarioGuardado = usuarioService.guardarUsuario(usuario, usuarioRoles);
+	System.out.println(usuarioGuardado.getUsername());
 	}
 	
 	
